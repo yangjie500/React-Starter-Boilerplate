10393e9c359b821ecf92da00394e04014b168f94
Yang Jie <kristeh97@.gmail.com>
2021-12-25 18:10:19 +0800

    Set up my Own Starter React Typescript BoilerPlate

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./css/styles.css";
+import {App} from "./components/App";
+
+ReactDOM.render(<App num={12345}/>, document.getElementById("root"));
